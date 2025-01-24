e9ddf3b816b9834cea8dfd5663161275208c5c48
Yolokos <mixan19931@gmail.com>
2025-01-24 16:10:17 +0200

    Added SQL script for creating Trip table

---
+++ DevelopsTodayTask/Scripts/create_trip_table.sql
@@ -0,0 +1,34 @@
+-- Создание таблицы Trips
+CREATE TABLE Trips (
+    Id INTEGER NOT NULL CONSTRAINT "PK_Trips" PRIMARY KEY AUTOINCREMENT,
+    TpepPickupDatetime DATETIME NOT NULL,
+    TpepDropoffDatetime DATETIME NOT NULL,
+    PassengerCount INTEGER NOT NULL,
+    TripDistance REAL NOT NULL,
+    StoreAndFwdFlag TEXT NOT NULL,
+    PULocationID INTEGER NOT NULL,
+    DOLocationID INTEGER NOT NULL,
+    FareAmount REAL NOT NULL,
+    TipAmount REAL NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_unique_trip_times 
+ON TripsTest (TpepPickupDatetime, TpepDropoffDatetime);
+
+CREATE INDEX idx_pulocation_id
+ON TripsTest (PULocationID);
+
+CREATE INDEX idx_dolocation_id
+ON TripsTest (DOLocationID);
+
+CREATE INDEX idx_pickup_datetime
+ON TripsTest (TpepPickupDatetime);
+
+CREATE INDEX idx_dropoff_datetime
+ON TripsTest (TpepDropoffDatetime);
+
+CREATE INDEX idx_trip_distance
+ON TripsTest (TripDistance);
+
+CREATE INDEX idx_tip_amount
+ON TripsTest (TipAmount);
